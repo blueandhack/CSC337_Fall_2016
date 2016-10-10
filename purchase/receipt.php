db954401bc1813f8688c1c1d693c43df2af1253b
Yoga Lin <blueandhack@gmail.com>
2016-10-10 13:28:38 -0700

    updated project 06

--- purchase/receipt.php
+++ purchase/receipt.php
@@ -9,32 +9,32 @@
 <?php
 date_default_timezone_set('America/Phoenix');
 $receiptDate = date("d-M-Y");
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$zip = $_POST['zip'];
-$size = $_POST['size'];
-$number = $_POST['number'];
+$firstName = $_POST ['firstName'];
+$lastName = $_POST ['lastName'];
+$city = $_POST ['city'];
+$state = $_POST ['state'];
+$zip = $_POST ['zip'];
+$size = $_POST ['size'];
+$quantity = $_POST ['quantity'];
 $value = 0;
 $total = 0;
 if ($size == 'small') {
     $value = 2.00;
-    $total = $value * $number;
+    $total = $value * $quantity;
 } else if ($size = 'medium') {
     $value = 2.65;
-    $total = $value * $number;
+    $total = $value * $quantity;
 } else {
     $value = 2.99;
-    $total = $value * $number;
+    $total = $value * $quantity;
 }
 
 ?>
 <fieldset class="receipt-background">
     <h1>Receipt</h1>
     <p>
-        Purchase date:<?php echo $receiptDate; ?><br>
-        Purchase <?php echo $number ?> item(s) of size '<?php echo $size ?>' at <?php echo $value ?> ecah<br>
+        Purchase date:&nbsp;<?php echo $receiptDate; ?><br>
+        Purchase <?php echo $quantity ?> item(s) of size '<?php echo $size ?>' at <?php echo $value ?> each<br>
         Total Cost: <?php echo $total ?><br>
     </p>
 
