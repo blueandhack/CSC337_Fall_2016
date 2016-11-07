9e99d0c265351f2be3434b46eaaa6127b26da29b
Yoga Lin <blueandhack@gmail.com>
2016-11-07 01:06:39 -0700

    updated quotes sql

--- quotations/quotes.sql
+++ quotations/quotes.sql
@@ -1,30 +1,35 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: localhost (MySQL 5.6.28)
-# Database: quotes
-# Generation Time: 2016-11-06 06:23:04 +0000
-# ************************************************************
-
+-- MySQL dump 10.13  Distrib 5.6.28, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: quotes
+-- ------------------------------------------------------
+-- Server version	5.6.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Current Database: `quotes`
+--
 
-# Dump of table quotations
-# ------------------------------------------------------------
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `quotes` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `quotes`;
+
+--
+-- Table structure for table `quotations`
+--
 
 DROP TABLE IF EXISTS `quotations`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `quotations` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `quote` text,
@@ -32,26 +37,26 @@ CREATE TABLE `quotations` (
   `vote` int(11) NOT NULL DEFAULT '0',
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quotations`
+--
 
 LOCK TABLES `quotations` WRITE;
 /*!40000 ALTER TABLE `quotations` DISABLE KEYS */;
-
-INSERT INTO `quotations` (`id`, `quote`, `author`, `vote`, `date`)
-VALUES
-	(4,'Nothing has more strength than dire necessity.','Euripides',-5,'2016-11-05 23:06:02'),
-	(5,'Fortune and love favor the brave.','Ovid',-5,'2016-11-05 23:05:18'),
-	(6,'Great artists suffer for the people. ','Marvin Gaye',-6,'2016-11-05 23:05:32'),
-	(7,'I\'d rather have roses on my table than diamonds on my neck. ','Emma Goldman',1,'2016-11-05 23:04:58');
-
+INSERT INTO `quotations` VALUES (4,'Nothing has more strength than dire necessity.','Euripides',-2,'2016-11-06 18:22:40'),(5,'Fortune and love favor the brave.','Ovid',-2,'2016-11-06 18:22:31'),(6,'Great artists suffer for the people. ','Marvin Gaye',-6,'2016-11-05 23:05:32'),(7,'I\'d rather have roses on my table than diamonds on my neck. ','Emma Goldman',3,'2016-11-06 18:26:50'),(8,'a','a',2,'2016-11-06 18:34:59'),(9,'abc','abc',1,'2016-11-06 18:34:57');
 /*!40000 ALTER TABLE `quotations` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-06 22:58:34
