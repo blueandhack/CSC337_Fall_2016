8145491c3d53b502f90a8e99295965f9b9f72f37
Yoga Lin <blueandhack@gmail.com>
2016-12-02 12:27:22 -0700

    split php and javascript

--- ajax_search/index.php
+++ ajax_search/index.php
@@ -17,70 +17,8 @@ Title: <input type="text" id="movie_name" onkeyup="getMovies()">
 Actor: <input type="text" id="actor_name" onkeyup="getMovies()"> (last name, first name)
 <div id="divToChange"></div>
 
-<script>
-    function getMovies() {
-        var getMovieName = document.getElementById("movie_name").value;
-        var getFirstNameAndLastName = document.getElementById("actor_name").value;
+<script type="text/javascript" src="all.js"></script>
 
-        var getLastName = "";
-        var getFirstName = "";
-
-        if (getFirstNameAndLastName.split(", ").length == 2) {
-            var name = getFirstNameAndLastName.split(", ");
-            getLastName = name[0];
-            getFirstName = name[1];
-        } else if (getFirstNameAndLastName.split(", ").length == 1 && getFirstNameAndLastName.search(", ") == -1) {
-            if (getFirstNameAndLastName.charAt(getFirstNameAndLastName.length - 1) == ',') {
-                getLastName = getFirstNameAndLastName.substring(0, getFirstNameAndLastName.length - 1);
-            } else {
-                getLastName = getFirstNameAndLastName;
-            }
-        } else if (getFirstNameAndLastName.split(", ").length == 1 && getFirstNameAndLastName.search(", ") != -1) {
-
-            getFirstName = getFirstNameAndLastName;
-
-        }
-
-        if ((getMovieName == "" && getFirstNameAndLastName == "") || (getMovieName == "" && getFirstNameAndLastName == ", ")) {
-            document.getElementById("divToChange").innerHTML = "";
-        } else {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    var array = JSON.parse(xhttp.responseText);
-
-                    var str = "<br>Results for those keywords ";
-                    if (getMovieName != "") {
-                        str += " Title:'" + getMovieName + "' ";
-                    }
-                    if (getFirstName != "") {
-                        str += " First Name:'" + getFirstName + "' ";
-                    }
-                    if (getLastName != "") {
-                        str += " Last Name:'" + getLastName + "' ";
-                    }
-
-                    str += "<br><br>";
-
-                    if (array.length == 0) {
-                        str += "Sorry! We found nothing!";
-                    } else {
-                        str += "<table><tr><th>Title</th><th>Actor</th><th>Year</th></tr>";
-                        for (var i = 0; i < array.length; i++) {
-                            str += "<tr><td>" + array[i]["name"] + "</td><td>" + array[i]["first_name"] + ' ' + array[i]["last_name"] + "</td><td>" + array[i]["year"] + '</td><tr>';
-                        }
-                    }
-                    str += "</table>";
-                    document.getElementById("divToChange").innerHTML = str;
-
-                }
-            };
-            xhttp.open("GET", "controller.php?movie_name=" + getMovieName + "&first_name=" + getFirstName + "&last_name=" + getLastName, true);
-            xhttp.send();
-        }
-
-    }
-</script>
 
 </body>
 </html>
